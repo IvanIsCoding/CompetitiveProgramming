df02f446bffe54a18f72e4c05171d97f078edc8f
Ivan Carvalho <ivan.ivancps.cn@gmail.com>
2018-05-01 20:51:49 -0300

    Create 1645.cpp

---
+++ URI/1645.cpp
@@ -0,0 +1,25 @@
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+typedef long long ll;
+ll dp[102][102],vetor[102],n,k;
+ll solve(ll pos,ll tamanho){
+	if(tamanho == k) return 1;
+	if(dp[pos][tamanho] != -1) return dp[pos][tamanho];
+	dp[pos][tamanho] = 0;
+	for(ll nxt=pos+1;nxt<=n;nxt++){
+		if(vetor[pos] < vetor[nxt]) dp[pos][tamanho] += solve(nxt,tamanho+1);
+	}
+	return dp[pos][tamanho];
+}
+int main(){
+	while(scanf("%lld %lld",&n,&k) &&(n||k)){
+		memset(dp,-1,sizeof(dp));
+		for(ll i=1;i<=n;i++) scanf("%lld",&vetor[i]);
+		ll resp = 0;
+		for(ll i=1;i<=n;i++) resp += solve(i,1);
+		printf("%lld\n",resp);
+	}
+	return 0;
+}
