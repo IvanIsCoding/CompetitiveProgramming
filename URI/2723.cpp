b56d1f2b461e1b5f383a6f11ec6a18a6031580e2
Ivan Carvalho <ivan.ivancps.cn@gmail.com>
2018-05-01 20:38:36 -0300

    Create 2723.cpp

---
+++ URI/2723.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int MAXN = 1e4 + 10;
+int dp[MAXN][7][7],n,vetor[MAXN],TC;
+inline int solve(int i,int a,int b){
+	int c = min(a,b);
+	a -= c;b -= c;
+	if(a < b) swap(a,b);
+	if(a - b > 5) return 0;
+	if(dp[i][a][b] != -1){
+		return dp[i][a][b];
+	}
+	if(i ==  n + 1) return dp[i][a][b] = 1;
+	return dp[i][a][b] = solve(i+1,a+vetor[i],b) || solve(i+1,a,b+vetor[i]);
+}
+int main(){
+	scanf("%d",&TC);
+	while(TC--){
+		scanf("%d",&n);
+		memset(dp,-1,sizeof(dp));
+		for(int i = 1;i<=n;i++){
+			scanf("%d",&vetor[i]);
+		}
+		if(solve(1,0,0)) printf("Feliz Natal!\n");
+		else printf("Ho Ho Ho!\n");
+	}
+	return 0;
+}
