bb82d9ec5057a50afd981e855bdee1a862c8a77a
Ivan Carvalho <ivan.ivancps.cn@gmail.com>
2018-05-01 20:43:57 -0300

    Create 2680.cpp

---
+++ URI/2680.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main(){
+	ll n;
+	scanf("%lld",&n);
+	while(n--){
+		ll x;
+		scanf("%lld",&x);
+		ll copia = x;
+		ll soma = 1;
+		for(ll i = 2;i*i<=x;i++){
+			if(copia % i == 0){
+				ll tot = 1;
+				ll pot = i;
+				while(copia % i == 0){
+					copia /= i;
+					tot += pot;
+					pot *= i;
+				}
+				soma *= tot;
+			}
+		}
+		if(copia != 1) soma = soma*(copia + 1);
+		printf("%lld\n",soma);
+	}
+	return 0;
+}
