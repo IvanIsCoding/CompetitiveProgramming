e5d06765b34a06c9ab3b33127c9457913f619bff
Ivan Carvalho <ivan.ivancps.cn@gmail.com>
2018-05-01 21:11:05 -0300

    Create 2539.cpp

---
+++ URI/2539.cpp
@@ -0,0 +1,35 @@
+#include <cstdio>
+#include <cstring>
+#define LSOne(S) (S & (-S))
+const int MAXN = 1e5 + 1;
+int bit[MAXN],vetor[MAXN],n;
+void update(int idx){
+	while(idx < MAXN){
+		bit[idx]++;
+		idx += LSOne(idx);
+	}
+}
+int query(int idx){
+	int ans = 0;
+	while(idx > 0){
+		ans += bit[idx];
+		idx -= LSOne(idx);
+	}
+	return ans;
+}
+int main(){
+	while(scanf("%d",&n) != EOF){
+		memset(bit,0,sizeof(bit));
+		for(int i=1;i<=n;i++){
+			scanf("%d",&vetor[n - i + 1]);
+		}
+		long long resp = 0;
+		for(int i=1;i<=n;i++){
+			int davez = query(n) - query(vetor[i]-1);
+			resp += (long long) davez;
+			update(vetor[i]);
+		}
+		printf("%lld\n",resp);
+	}
+	return 0;
+}
