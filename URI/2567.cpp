f68deff672c72a1b1348e49f3d78acd79ed9e894
Ivan Carvalho <ivan.ivancps.cn@gmail.com>
2018-05-01 21:02:37 -0300

    Create 2567.cpp

---
+++ URI/2567.cpp
@@ -0,0 +1,25 @@
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int main(){
+	int n;
+	while(scanf("%d",&n) != EOF){
+		vector<int> entrada;
+		for(int i=0;i<n;i++){
+			int davez;
+			scanf("%d",&davez);
+			entrada.push_back(davez);
+		}
+		sort(entrada.begin(),entrada.end());
+		int resp = 0;
+		int ini = 0,fim = n-1;
+		while(ini<=fim){
+			resp += entrada[fim] - entrada[ini];
+			ini++;
+			fim--;
+		}
+		printf("%d\n",resp);
+	}
+	return 0;
+}
