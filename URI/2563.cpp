1494427583976e0fdcca8e181b650225c8cec199
Ivan Carvalho <ivan.ivancps.cn@gmail.com>
2018-05-01 21:12:03 -0300

    Create 2563.cpp

---
+++ URI/2563.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+const int MAXP = 1e4 + 1;
+int melhor[MAXP],D[MAXP],P[MAXP],n,cap;
+int main(){
+	while(scanf("%d %d",&n,&cap) != EOF){
+		memset(melhor,0,sizeof(melhor));
+		for(int i=1;i<=n;i++) scanf("%d",&D[i]);
+		for(int i=1;i<=n;i++) scanf("%d",&P[i]);
+		for(int i=1;i<=n;i++){
+			for(int j = MAXP-1;j >= P[i];j--){
+				melhor[j] = max(melhor[j], melhor[j - P[i]]  + D[i]);
+			}
+		}
+		int resp = 0;
+		for(int i=0;i<=cap;i++) resp = max(resp,melhor[i]);
+		printf("%d\n",resp);
+	}
+	return 0;
+}
