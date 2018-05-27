4900099bd1e10413bc51c52e380d5b16102f5081
Ivan Carvalho <ivan.ivancps.cn@gmail.com>
2018-05-27 15:24:53 -0300

    More DMOJ Solutions

---
+++ DMOJ/inaho4.py
@@ -0,0 +1,7 @@
+n = int(raw_input())
+a = [float(i) for i in raw_input().split()]
+b = [float(i) for i in raw_input().split()]
+total = 0
+for i in xrange(len(a)):
+	total += (b[i] - a[i])*(b[i] - a[i])
+print "%.6f" % (total**0.5)
