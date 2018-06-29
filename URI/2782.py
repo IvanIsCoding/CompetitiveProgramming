2d01ec7705393b35eb32d22e620ece338c04e823
Ivan Carvalho <ivan.ivancps.cn@gmail.com>
2018-06-29 10:13:24 -0300

    OBI 2018

---
+++ URI/2782.py
@@ -0,0 +1,28 @@
+# -*- coding : utf-8 -*-
+
+N = int(input())
+vetor = [int(i) for i in input().split()]
+
+intervalos = set()
+
+for i in range(N):
+	if i >= 1:
+		ini = i
+		fim = i
+		diferenca = vetor[i] - vetor[i-1]
+		while ini >= 1 and vetor[ini] - vetor[ini-1] == diferenca:
+			ini -= 1
+		while fim + 1 < N and vetor[fim + 1] - vetor[fim] == diferenca:
+			fim += 1
+		intervalos.add((ini,fim))
+	if i + 1 < N:
+		ini = i
+		fim = i
+		diferenca = vetor[i+1] - vetor[i]
+		while ini >= 1 and vetor[ini] - vetor[ini-1] == diferenca:
+			ini -= 1
+		while fim + 1 < N and vetor[fim + 1] - vetor[fim] == diferenca:
+			fim += 1
+		intervalos.add((ini,fim))
+
+print(max(len(intervalos),1))
