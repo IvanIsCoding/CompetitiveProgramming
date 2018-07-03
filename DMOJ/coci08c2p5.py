c20480c890f6f1400e970abed3fe201e8ac5391c
Ivan <ivan@IMAC-HAC.local>
2018-07-02 23:16:41 -0300

    COCI

---
+++ DMOJ/coci08c2p5.py
@@ -0,0 +1,19 @@
+# -*- coding : utf-8 -*-
+
+entrada = input()
+
+somatorio = 1
+nos = 1
+
+for caractere in entrada:
+	if caractere == "P":
+		continue
+	elif caractere == "L":
+		somatorio = somatorio * 2
+	elif caractere == "R":
+		somatorio = somatorio*2 + nos
+	else:
+		somatorio = somatorio*5 + nos
+		nos *= 3
+
+print(somatorio)
